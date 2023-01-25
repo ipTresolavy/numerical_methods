9f60b4132496ab2ba4ff6b8158e58d22253a4ffd
Thiago souza <thiagosouza@Thiagos-Laptop.local>
2023-01-25 11:55:45 -0300

    tarefa 3

---
+++ tasks/03/scripts/1_questao_computacional.py
@@ -0,0 +1,16 @@
+# autor: Igor Pontes Tresolavy
+# autor: Thiago Antici Rodrigues de Souza
+"""
+Usando o metodo de Euler Implicito em conjunto com o metodo das aproximacoes para resolver
+
+dy/dt = e^t-2y, 0<t<1, y(0= ln(2)/2
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#DEFINICOES
+INICIO_INTERVALO=0
+FIM_INTERVALO=1
+
