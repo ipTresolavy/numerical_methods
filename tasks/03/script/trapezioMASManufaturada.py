2ff1107a91d57d72389aa13cd078cc187c446658
Igor Pontes Tresolavy <tresolavy@usp.br>
2023-02-24 11:57:15 -0300

    atualiza problema manufaturado

--- tasks/03/script/trapezioMASManufaturada.py
+++ tasks/03/script/trapezioMASManufaturada.py
@@ -3,17 +3,47 @@
 
 """
 Confeccionando tabelas de convergência para o problema manufaturado:
-y_e(t) = e^(-t) - e^(-4t)
+y_e(t) = (y_1(t), y_2(t)) =
+(
+e^(-t)*cos(t),
+6e^(-t)sin(t)
+)
 
 Problema de Cauchy 2D
-d[(y1(t), y2(t))]/dt = ((0, 1), (-4, -5))*(y1(t), y2(t))
-(y1(0), y2(0)) = (0, 3)
+d[(y1(t), y2(t))]/dt = ((-1, -1/6), (6, -1))*(y1(t), y2(t))
+(y_1(0), y_2(0)) = (1, 0)
 
 Há 9 parâmetros nesse programa:
-    NO_DE_CASOS: define a quantidade de linhas na tabela da saída. Ou seja, define quantas malhas progressivamente mais finas que o programa executa.
-    INICIO_INTERVALO: define o início do intervalo o qual se deseja discretizar através o Método do Trapézio Implícito
 
-Adicionalmente, pode-se alterar a função f do Problema de Cauchy e a solução conhecida a fim de reutilizar o programa para a discretização de outro problema
+    NO_DE_CASOS: define a quantidade de linhas na tabela da saída.
+                 Ou seja, define quantas malhas progressivamente
+                 mais finas que o programa executa.
+
+    INICIO_INTERVALO: define o início do intervalo o qual se deseja
+                      discretizar a solução da EDO através o Método
+                      do Trapézio Implícito
+
+    CONDICAO_INICIAL: condição inicial do Problema de Cauchy
+
+    FIM_INTERVALO: define o fim do intervalo o qual se deseja
+                   discretizar a solução da EDO através o Método
+                   do Trapézio Implícito
+
+    QNTD_PASSOS_INICIAL: define a primeira quantidade de subintervalos
+                         no intervalo [INICIO_INTERVALO, FIM_INTERVALO]
+
+    FATOR_MULTIPLICATIVO: define em quantas vezes a quantidade de
+                          subintervalos é diminuída a cada caso
+
+    TOL: define a tolerância do erro relativo no critério de para do
+         Método do Ponto Fixo
+
+    MAXIMO_ITERACOES: define a quantidade máxima de iterações do
+                      Método do Ponto Fixo
+
+Adicionalmente, pode-se alterar a função f do Problema de Cauchy e a
+solução conhecida a fim de reutilizar o programa para a discretização
+de outro problema
 """
 
 import matplotlib.pyplot as plt
@@ -22,7 +52,7 @@ import numpy as np
 # parâmetros
 NO_DE_CASOS = 10
 INICIO_INTERVALO = 0
-CONDICAO_INICIAL = [0, 3]
+CONDICAO_INICIAL = [1, 0]
 FIM_INTERVALO = (3.0*np.pi)/4.0
 QNTD_PASSOS_INICIAL = 8
 FATOR_MULTIPLICATIVO = 2
@@ -32,11 +62,11 @@ MAXIMO_ITERACOES = 3
 
 # Solução exata do Problema de Cauchy 2D
 def y_e(T):
-    return np.array([np.exp(-T) - np.exp(-4*T), -np.exp(-T) + 4*np.exp(-4*T)])
+    return np.array([np.exp(-T)*np.cos(T), 6*np.exp(-T)*np.sin(T)])
 
 # f(t,y_1, y_2) do problema de Cauchy 2D
 def f(t, y):
-    return np.array([y[1], -4*y[0] -5*y[1]])
+    return np.array([-1*y[0] -y[1]/6, 6*y[0] -1*y[1]])
 
 # Método do Trapézio Implícito com Iterações de Newton
 def trapezoidal(t_0, T, h_n, f, w_0):
@@ -99,22 +129,22 @@ def main():
 
         # traçado de gráficos para determinados casos
         if caso == 2 or caso == 3 or caso == 8:
-            ax1.plot(malha__do__tempo, [y[0] for y in aproximacao__numerica], color="black", linestyle=linestyles[caso%4], label="$\eta_1(t,h)$ para n = {}".format(str(n)))
-            ax2.plot(malha__do__tempo, [y[1] for y in aproximacao__numerica], color="black", linestyle=linestyles[caso%4], label="$\eta_2(t,h)$ para n = {}".format(str(n)))
+            ax1.plot(malha__do__tempo, [y[0] for y in aproximacao__numerica], color="black", linestyle=linestyles[caso%4], label="n = {}".format(str(n)))
+            ax2.plot(malha__do__tempo, [y[1] for y in aproximacao__numerica], color="black", linestyle=linestyles[caso%4], label="n = {}".format(str(n)))
 
 
 
-    ax1.plot(malha__do__tempo, y_e(malha__do__tempo)[0], color="black", linestyle="solid", label="solução exata")
-    ax2.plot(malha__do__tempo, y_e(malha__do__tempo)[1], color="black", linestyle="solid", label="primeira derivada da solução exata")
+    ax1.plot(malha__do__tempo, y_e(malha__do__tempo)[0], color="black", linestyle="solid", label="solução exata $y_1(t)$")
+    ax2.plot(malha__do__tempo, y_e(malha__do__tempo)[1], color="black", linestyle="solid", label="solução exata $y_2(t)$")
 
     ax1.set_xlabel("t (adimensional)")
     ax1.set_ylabel("variável de estado (adimensional)")
-    ax1.set_title("Método de Euler 2D para a variável de estado $y_1$ \n(solução exata $y_e(t) = e^{-t} - e^{-4t}$)", size=12)
+    ax1.set_title("Método do Trapézio Implícito para a variável de estado $y_1$ \n(solução exata $y_1(t) = e^{-t}*cos(t)$)", size=12)
     ax1.legend(loc="upper right",)
 
     ax2.set_xlabel("t (adimensional)")
     ax2.set_ylabel("variável de estado (adimensional)")
-    ax2.set_title("Método de Euler 2D para a variável de estado $y_2$ \n(derivada da solução exata $\dot{y}_e(t) = -e^{-t} + 4e^{-4t}$)", size=12)
+    ax2.set_title("Método do Trapézio Implícito para a variável de estado $y_2$ \n(solução exata $y_2(t) = 6e^{-t}*sin(t)$)", size=12)
     ax2.legend(loc="upper right")
 
     ax1.set_box_aspect(1/2)
