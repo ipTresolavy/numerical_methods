b9339c06ba767d6ef4f0339987f6728716a94de3
Thiago souza <thiagosouza@Thiagos-Laptop.local>
2023-02-24 18:51:02 -0300

    feat: standardized lines

--- tasks/03/script/newtonOrdemRungeKutta.py
+++ tasks/03/script/newtonOrdemRungeKutta.py
@@ -73,23 +73,21 @@ def main():
     regiaoRK33 = regiaoDeEstabilidade(RK33)
     regiaoRK44 = regiaoDeEstabilidade(RK44)
 
+    # Realizamos a ordenacao do plano para que
+    # eles as linhas sejam displayed corretamente
     ordenarPontos(regiaoRK11)
     ordenarPontos(regiaoRK22)
     ordenarPontos(regiaoRK33)
     ordenarPontos(regiaoRK44)
 
-    np.sort(regiaoRK22)
-    np.sort(regiaoRK33)
-    np.sort(regiaoRK44)
-
     ax.plot([z.real for z in regiaoRK11], [
-            z.imag for z in regiaoRK11], color="blue", label="RK11")
+            z.imag for z in regiaoRK11], linestyle="dotted", color="black", label="RK11")
     ax.plot([z.real for z in regiaoRK22], [z.imag for z in regiaoRK22],
-            color="green", linestyle="solid", label="RK22")
+            color="black", linestyle="dashed", label="RK22")
     ax.plot([z.real for z in regiaoRK33], [z.imag for z in regiaoRK33],
-            color="orange", linestyle="solid", label="RK33")
+            color="black", linestyle="solid", label="RK33")
     ax.plot([z.real for z in regiaoRK44], [z.imag for z in regiaoRK44],
-            color="red", linestyle="solid", label="RK44")
+            color="black", linestyle=(0, (3, 5, 1, 5)), label="RK44")
     ax.set_xlabel("$Re(\lambda h)$")
     ax.set_ylabel("$Im(\lambda h)$")
     ax.set_title("Regiões de Estabilidade para Métodos Runge-Kutta")
@@ -98,17 +96,23 @@ def main():
     fig.tight_layout()
     plt.show()
 
+# Funcao responsavel por transaldar em 1 unidade para o centro do plano
+
 
 def transladar_1_positivo(regiao):
     for i in range(len(regiao)):
         novo = np.add(regiao[i], 1)
         regiao[i] = novo
 
+# Funcao responsavel por transaldar em 1 unidade para o offset do plano
+
 
 def transladar_1_negativo(regiao):
     for i in range(len(regiao)):
         novo = np.add(regiao[i], -1)
         regiao[i] = novo
+
+# Funcao responsavel por fazer a ordenacao dos pontos nas regioes de convergencia
 
 
 def ordenarPontos(regiao):
